6675f4aed785be5b118e13ba28e79a67f78bff3b
Leonid Poluyanov <lpoluyanov@ebay.com>
2015-03-27 17:23:34 +0200

    MAGETWO-35177: Create triggers for defined tables

--- tests/integration/testsuite/Migration/Step/DeltaTest.php
+++ tests/integration/testsuite/Migration/Step/DeltaTest.php
@@ -8,7 +8,6 @@ namespace Migration\Step;
 
 /**
  * Delta step test class
- * @dbFixture default
  */
 class DeltaTest extends \PHPUnit_Framework_TestCase
 {
@@ -21,11 +20,7 @@ class DeltaTest extends \PHPUnit_Framework_TestCase
         $migrate = $objectManager->create('\Migration\Step\Map\Migrate');
         $volume = $objectManager->create('\Migration\Step\Map\Volume');
         $delta = $objectManager-> create('\Migration\Step\Map\Delta');
-        /** @var  $source \Migration\Resource\Source */
         $source = $objectManager->create('\Migration\Resource\Source');
-        /** @var \Migration\Resource\Adapter\Mysql $mysqlAdapter */
-
-        /** @var \Symfony\Component\Console\Output\ConsoleOutput $progressBar */
         $map = $objectManager->create(
             '\Migration\Step\Map',
             [
@@ -35,14 +30,15 @@ class DeltaTest extends \PHPUnit_Framework_TestCase
                 'delta' => $delta
             ]
         );
+
         ob_start();
         $this->assertTrue($map->setUpChangeLog());
         ob_end_clean();
+
         $dataTable = 'table_with_data';
         $changeLogTableName = $source->getChangeLogName($dataTable);
         $changeLogTable = $source->getDocument($changeLogTableName);
         $this->assertEquals($changeLogTableName, $changeLogTable->getName());
-        /** @var \Migration\Resource\Document $sourceTables */
         $sourceAdapter = $source->getAdapter();
         $sourceAdapter->insertRecords(
             $dataTable,
