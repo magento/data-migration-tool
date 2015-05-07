645af66b40a7e21d000bec44afa997ad9b829f8b
Alex Bomko <abomko@ebay.com>
2015-05-07 20:55:34 +0300

    MAGETWO-36547: [Migration] Tool failed if try run it from bin folder

--- tests/integration/framework/Helper.php
+++ tests/integration/framework/Helper.php
@@ -103,7 +103,16 @@ class Helper
             $this->objectManager = $this->initObjectManager();
         }
         $this->objectManager->configure([
-            'preferences' => ['\Migration\App\ProgressBar\LogLevelProcessor' => '\Migration\TestFramework\ProgressBar']
+            'preferences' => ['Migration\App\ProgressBar\LogLevelProcessor' => 'Migration\TestFramework\ProgressBar'],
+            'Migration\Logger\Logger' => [
+                'arguments' => [
+                    'handlers' => [
+                        'quiet' => [
+                            'instance' => '\Migration\TestFramework\QuietLogHandler'
+                        ]
+                    ]
+                ]
+            ]
         ]);
         return $this->objectManager;
     }
