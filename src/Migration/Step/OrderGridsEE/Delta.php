75ceae793a31bb216dee874476d89c284e025a7f
Alex Bomko <abomko@ebay.com>
2015-07-16 19:00:33 +0300

    MAGETWO-35786: Migration tool maintenance to reflect actual state of M2 data scheme for all versions of M1

--- src/Migration/Step/OrderGridsEE/Delta.php
+++ src/Migration/Step/OrderGridsEE/Delta.php
@@ -35,4 +35,44 @@ class Delta extends \Migration\Step\OrderGrids\Delta
         $this->data = $data;
         parent::__construct($source, $destination, $groupsFactory, $logger, $helper, $data);
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function perform()
+    {
+        parent::perform();
+
+        $documentMap = $this->helper->getDocumentList();
+        $sourceDocuments = $this->readerGroups->getGroup('archive_orders');
+        foreach ($sourceDocuments as $sourceDocName => $idKey) {
+            if ($this->source->getRecordsCount($this->source->getDeltaLogName($sourceDocName)) == 0) {
+                continue;
+            }
+            $items = $this->source->getDeletedRecords($sourceDocName, $idKey, 0, true);
+            if (empty($items)) {
+                continue;
+            }
+            $this->logger->debug(sprintf('%s has changes', $sourceDocName));
+
+            if (!$this->eolOnce) {
+                $this->eolOnce = true;
+                echo PHP_EOL;
+            }
+            $page = 1;
+
+            $destinationDocument = $documentMap[$sourceDocName];
+            do {
+                $this->destination->deleteRecords(
+                    $this->destination->addDocumentPrefix($destinationDocument),
+                    $idKey,
+                    $items
+                );
+                $documentNameDelta = $this->source->getDeltaLogName($sourceDocName);
+                $documentNameDelta = $this->source->addDocumentPrefix($documentNameDelta);
+                $this->markRecordsProcessed($documentNameDelta, $idKey, $items);
+            } while (!empty($items = $this->source->getChangedRecords($sourceDocName, $idKey, $page++)));
+        }
+        return true;
+    }
 }
