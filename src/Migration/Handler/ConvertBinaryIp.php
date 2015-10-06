dec6e27b729aef0903856853c5e51263742cce1c
Ruslan Kostiv <rkostiv@ebay.com>
2015-10-06 18:59:48 +0300

    MAGETWO-43480: Implement migration from latest CE versions to M2.0 EE

---
+++ src/Migration/Handler/ConvertBinaryIp.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Migration\Handler;
+
+use Migration\Resource\Record;
+
+/**
+ * Handler to set hash value to the field, based on other field
+ */
+class ConvertBinaryIp extends AbstractHandler implements HandlerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Record $recordToHandle, Record $oppositeRecord)
+    {
+        $this->validate($recordToHandle);
+
+        $value      = $recordToHandle->getValue($this->field);
+        $newValue   = ip2long(inet_ntop($value));
+        $recordToHandle->setValue($this->field, $newValue);
+    }
+}
---
+++ tests/unit/testsuite/Migration/Handler/ConvertBinaryIpTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Migration\Handler;
+
+class ConvertBinaryIpTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $ip         = '127.0.0.1';
+        $ipDbValue  = inet_pton($ip);
+
+        $fieldName  = 'fieldname';
+
+        /** @var \Migration\Resource\Record|\PHPUnit_Framework_MockObject_MockObject $record */
+        $record = $this->getMock('Migration\Resource\Record', ['setValue', 'getValue', 'getFields'], [], '', false);
+        $record->expects($this->any())->method('getFields')->willReturn([$fieldName]);
+        $record->expects($this->any())->method('getValue')->with($fieldName)->willReturn($ipDbValue);
+        $record->expects($this->any())->method('setValue')->with($fieldName, ip2long(inet_ntop($ipDbValue)));
+
+        $record2 = $this->getMockBuilder('Migration\Resource\Record')->disableOriginalConstructor()->getMock();
+
+        $handler = new ConvertBinaryIp();
+        $handler->setField($fieldName);
+        $handler->handle($record, $record2);
+    }
+}
