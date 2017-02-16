07750eb19e9586cbb0f312fadd14a461edddc7ad
Victor Rad <vrad@magento.com>
2017-02-16 12:40:36 +0200

    MAGETWO-63082: Update Data Migration Tool copyright section

--- tests/integration/resource/data_integrity/source.sql
+++ tests/integration/resource/data_integrity/source.sql
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2016 Magento. All rights reserved.
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
