a839e0bdf8818cde8f9483af4f9f68c80a88e003
Victor Rad <vrad@magento.com>
2018-01-12 10:35:37 +0200

    MAGETWO-81750: Duplicate entry error on customer_group table when bulk_size is 1

--- tests/unit/testsuite/Migration/ResourceModel/SourceTest.php
+++ tests/unit/testsuite/Migration/ResourceModel/SourceTest.php
@@ -42,6 +42,16 @@ class SourceTest extends \PHPUnit\Framework\TestCase
     protected $structureFactory;
 
     /**
+     * @var \Migration\ResourceModel\Document|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $document;
+
+    /**
+     * @var \Migration\ResourceModel\Structure|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $structure;
+
+    /**
      * @var \Migration\ResourceModel\Document\Collection|\PHPUnit_Framework_MockObject_MockObject
      */
     protected $documentCollection;
@@ -63,7 +73,17 @@ class SourceTest extends \PHPUnit\Framework\TestCase
         );
         $this->adapter = $this->createPartialMock(
             \Migration\ResourceModel\Adapter\Mysql::class,
-            ['select', 'fetchAll', 'query', 'loadPage', 'createDelta', 'loadChangedRecords', 'loadDeletedRecords']
+            [
+                'select',
+                'fetchAll',
+                'query',
+                'loadPage',
+                'createDelta',
+                'loadChangedRecords',
+                'loadDeletedRecords',
+                'getDocumentStructure',
+                'getRecords'
+            ]
         );
         $this->adapterFactory = $this->createPartialMock(
             \Migration\ResourceModel\AdapterFactory::class,
@@ -74,9 +94,19 @@ class SourceTest extends \PHPUnit\Framework\TestCase
             ->with($adapterConfigs)
             ->will($this->returnValue($this->adapter));
         $this->documentFactory = $this->getMockBuilder(\Migration\ResourceModel\DocumentFactory::class)
+            ->setMethods(['create'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->document = $this->getMockBuilder(\Migration\ResourceModel\Document::class)
+            ->setMethods(['getStructure'])
             ->disableOriginalConstructor()
             ->getMock();
         $this->structureFactory = $this->getMockBuilder(\Migration\ResourceModel\StructureFactory::class)
+            ->setMethods(['create'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->structure = $this->getMockBuilder(\Migration\ResourceModel\Structure::class)
+            ->setMethods(['getFields'])
             ->disableOriginalConstructor()
             ->getMock();
         $this->documentCollection = $this->createMock(\Migration\ResourceModel\Document\Collection::class);
@@ -148,5 +178,40 @@ class SourceTest extends \PHPUnit\Framework\TestCase
             ['init_statements_source', 'SET NAMES utf8;']
         ]);
         $this->resourceSource->getDeletedRecords('document', 'key_field');
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetRecordsWithOneBulkSize()
+    {
+        $document = 'doc1';
+        $structureData = ['id' => 'int'];
+        $records = [['id' => 0, 'field1' => 'data']];
+        $fields = ['id' => ['PRIMARY' => true, 'IDENTITY' => true, 'COLUMN_NAME' => 'id']];
+        $this->config->expects($this->any())->method('getOption')->willReturnMap([
+            ['edition_migrate', 'opensource-to-opensource'],
+            ['source_prefix', ''],
+            ['bulk_size', 1],
+            ['init_statements_source', 'SET NAMES utf8;']
+        ]);
+        $this->adapter
+            ->expects($this->any())
+            ->method('getDocumentStructure')
+            ->with($document)
+            ->willReturn($structureData);
+        $this->adapter->expects($this->any())->method('loadPage')->with($document, 0, 1, 'id', 1)->willReturn($records);
+        $this->structureFactory
+            ->method('create')
+            ->with(['documentName' => $document, 'data' => $structureData])
+            ->willReturn($this->structure);
+        $this->documentFactory
+            ->method('create')
+            ->with(['structure' => $this->structure, 'documentName' => $document])
+            ->willReturn($this->document);
+        $this->document->expects($this->any())->method('getStructure')->will($this->returnValue($this->structure));
+        $this->structure->expects($this->any())->method('getFields')->will($this->returnValue($fields));
+        $this->resourceSource->setLastLoadedRecord($document, $records[0]);
+        $this->assertEquals($records, $this->resourceSource->getRecords($document, 0));
     }
 }
