9fadaa1ca117cc35db6c32c5b30118c201204b4f
Victor Rad <vrad@magento.com>
2019-09-16 11:12:57 -0500

    MAGETWO-90635: [GitHub] Add ability for Data Migration Tool to load map files outside its own directory #495

--- src/Migration/Reader/Map.php
+++ src/Migration/Reader/Map.php
@@ -82,7 +82,7 @@ class Map implements MapInterface
 
         $configFile = file_exists($mapFile) ? $mapFile : $this->getRootDir() . $mapFile;
         if (!is_file($configFile)) {
-            throw new Exception('Invalid map filename: ' . $configFile);
+            throw new Exception('Invalid map filename: ' . $mapFile);
         }
 
         $xml = file_get_contents($configFile);
